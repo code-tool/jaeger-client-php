58f2ec090f9466ad3e9658654b4d32ae47b4eabd
allflame <allflame@syneforge.com>
2017-11-22 18:46:35 +0200

    Inject sample tags

--- src/Span/Factory/SpanFactory.php
+++ src/Span/Factory/SpanFactory.php
@@ -21,56 +21,41 @@ class SpanFactory implements SpanFactoryInterface
         $this->sampler = $sampler;
     }
 
-    public function sampleContext(string $operationName): SpanContext
-    {
-        $traceId = $this->idGenerator->next();
-        $spanId = $this->idGenerator->next();
-        $samplerResult = $this->sampler->decide($traceId, $operationName);
-        if (false === $samplerResult->isSampled()) {
-            return new SpanContext(
-                $traceId,
-                $spanId,
-                0,
-                0,
-                0,
-                []
-            );
-        }
-
-        return new SpanContext(
-            $traceId,
-            $spanId,
-            0,
-            0,
-            0x01,
-            $samplerResult->getTags()
-        );
-    }
-
-    public function createContext(string $operationName, SpanContext $parentContext = null): SpanContext
-    {
-        if (null === $parentContext) {
-            return $this->sampleContext($operationName);
-        }
-
-        return new SpanContext(
-            $parentContext->getTraceId(),
-            $this->idGenerator->next(),
-            $parentContext->getSpanId(),
-            $parentContext->getDebugId(),
-            $parentContext->getFlags(),
-            iterator_to_array($parentContext->getIterator())
-        );
-    }
-
     public function create(
         string $operationName,
         array $tags = [],
         SpanContext $parentContext = null,
         array $logs = []
     ): SpanInterface {
+        $spanId = $this->idGenerator->next();
+        if (null === $parentContext) {
+            $traceId = $this->idGenerator->next();
+            $debugId = 0;
+            $parentId = 0;
+            $flags = 0;
+            $baggage = [];
+            $samplerResult = $this->sampler->decide($traceId, $operationName);
+            if ($samplerResult->isSampled()) {
+                $flags = 0x01;
+                $tags = array_merge($tags, $samplerResult->getTags());
+            }
+        } else {
+            $traceId = $parentContext->getTraceId();
+            $parentId = $parentContext->getSpanId();
+            $debugId = $parentContext->getDebugId();
+            $flags = $parentContext->getFlags();
+            $baggage = [];
+        }
+
         return new Span(
-            $this->createContext($operationName, $parentContext),
+            new SpanContext(
+                $traceId,
+                $spanId,
+                $parentId,
+                $debugId,
+                $flags,
+                $baggage
+            ),
             $operationName,
             (int)round(microtime(true) * 1000000),
             $tags,
