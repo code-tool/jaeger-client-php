4a44c7dec96dcd5f74cce7f6603bc5cf18590d1b
allflame <allflame@syneforge.com>
2017-11-23 18:22:33 +0200

    Added codec registry

---
+++ src/Codec/CodecRegistry.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types=1);
+
+namespace CodeTool\OpenTracing\Codec;
+
+class CodecRegistry implements \ArrayAccess
+{
+    private $codecs = [];
+
+    public function offsetExists($offset)
+    {
+        return array_key_exists($offset, $this->codecs);
+    }
+
+    public function offsetGet($offset)
+    {
+        if (false === array_key_exists($offset, $this->codecs)) {
+            return null;
+        }
+
+        return $this->codecs[$offset];
+    }
+
+    public function offsetSet($offset, $value)
+    {
+        $this->codecs[$offset] = $value;
+
+        return $this;
+    }
+
+    public function offsetUnset($offset)
+    {
+        if (false === array_key_exists($offset, $this->codecs)) {
+            return $this;
+        }
+        unset($this->codecs[$offset]);
+
+        return $this;
+    }
+
+
+}
