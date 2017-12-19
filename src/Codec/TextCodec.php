d79883c9a2e8ef5f7c72dc145148965ac1d09757
allflame <allflame@syneforge.com>
2017-12-19 17:06:32 +0200

    Return from array

--- src/Codec/TextCodec.php
+++ src/Codec/TextCodec.php
@@ -7,6 +7,13 @@ use Jaeger\Span\Context\SpanContext;
 
 class TextCodec implements CodecInterface
 {
+    public function convertInt64(string $hex): int
+    {
+        $hex8byte = str_pad($hex, 16, '0', STR_PAD_LEFT);
+
+        return unpack('Jint64', pack('H*', $hex8byte))['int64'];
+    }
+
     public function decode($data): ?SpanContext
     {
         if (false === is_string($data)) {
@@ -18,10 +25,10 @@ class TextCodec implements CodecInterface
         }
 
         return new SpanContext(
-            (int)unpack('J', pack('H*', str_pad($elements[0], 16, '0', STR_PAD_LEFT))),
-            (int)unpack('J', pack('H*', str_pad($elements[1], 16, '0', STR_PAD_LEFT))),
-            (int)unpack('J', pack('H*', str_pad($elements[2], 16, '0', STR_PAD_LEFT))),
-            (int)unpack('J', pack('H*', str_pad($elements[3], 16, '0', STR_PAD_LEFT)))
+            $this->convertInt64($elements[0]),
+            $this->convertInt64($elements[1]),
+            $this->convertInt64($elements[2]),
+            $this->convertInt64($elements[3])
         );
     }
 
