db0cd2d1a2ffbbf3fbfdc8865d328cdffb6b10d3
allflame <allflame@syneforge.com>
2017-12-19 16:35:53 +0200

    Fix typo

--- src/Codec/TextCodec.php
+++ src/Codec/TextCodec.php
@@ -19,9 +19,9 @@ class TextCodec implements CodecInterface
 
         return new SpanContext(
             (int)unpack('J', pack('H*', $elements[0])),
-            (int)unpack('J', pack('H*', $elements[0])),
-            (int)unpack('J', pack('H*', $elements[0])),
-            (int)unpack('J', pack('H*', $elements[0]))
+            (int)unpack('J', pack('H*', $elements[1])),
+            (int)unpack('J', pack('H*', $elements[2])),
+            (int)unpack('J', pack('H*', $elements[3]))
         );
     }
 
