90d50c6438ba42a42b4ff82aec4665f9ec387bbb
allflame <allflame@syneforge.com>
2017-12-21 11:35:18 +0200

    PHP5 Compliance

--- src/Id/RandomIntGenerator.php
+++ src/Id/RandomIntGenerator.php
@@ -9,6 +9,6 @@ class RandomIntGenerator implements IdGeneratorInterface
      */
     public function next()
     {
-        return rand(0, PHP_INT_MAX);
+        return rand(-PHP_INT_MAX, PHP_INT_MAX);
     }
 }
