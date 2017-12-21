fdfb3942781e8f1a1d12f89ec0b57849d8365a22
allflame <allflame@syneforge.com>
2017-12-21 11:00:13 +0200

    PHP5 Compliance

--- src/Process/ProcessIpTag.php
+++ src/Process/ProcessIpTag.php
@@ -1,6 +1,4 @@
 <?php
-declare(strict_types=1);
-
 namespace Jaeger\Process;
 
 use Jaeger\Tag\StringTag;
@@ -9,6 +7,6 @@ class ProcessIpTag extends StringTag
 {
     public function __construct()
     {
-        parent::__construct('ip', $_SERVER['SERVER_ADDR'] ?? '127.0.0.1');
+        parent::__construct('ip', isset($_SERVER['SERVER_ADDR']) ?  $_SERVER['SERVER_ADDR']: '127.0.0.1');
     }
 }
