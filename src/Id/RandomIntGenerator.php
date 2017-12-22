462e3eef11bcd70edcedfcaa2b04476ec5b6cb24
allflame <allflame@syneforge.com>
2017-12-22 15:15:01 +0200

    Added random_compat for int generation in PHP5

--- src/Id/RandomIntGenerator.php
+++ src/Id/RandomIntGenerator.php
@@ -9,6 +9,6 @@ class RandomIntGenerator implements IdGeneratorInterface
      */
     public function next()
     {
-        return rand(0, PHP_INT_MAX);
+        return random_int(-PHP_INT_MAX + 1, PHP_INT_MAX);
     }
 }
