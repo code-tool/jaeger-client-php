a79efd87b6f20fa30560904e23b59e9b0fb8e6b8
allflame <allflame@syneforge.com>
2018-07-04 12:04:20 +0300

    Restrict buffer size

--- src/Client/ThriftClient.php
+++ src/Client/ThriftClient.php
@@ -12,7 +12,7 @@ use Jaeger\Thrift\Agent\AgentIf as AgentInterface;
 
 class ThriftClient implements ClientInterface
 {
-    const MAX_BATCH_SIZE = 100;
+    const MAX_BATCH_SIZE = 32;
 
     private $serviceName;
 
